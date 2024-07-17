b5be2b529bdfd8a41fd583fa92ce847170d127bd
armanoalves <armanobarrosalvesh@gmail.com>
2024-07-17 09:08:08 -0300

    Arquivos iniciais

---
+++ src/main/java/br/com/nerdops/api_nerdops/ApiNerdopsApplication.java
@@ -0,0 +1,13 @@
+package br.com.nerdops.api_nerdops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNerdopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNerdopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nerdops/api_nerdops/ApiNerdopsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nerdops.api_nerdops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNerdopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
